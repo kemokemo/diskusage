b0441f37628dee10a3ace419a7d7b68fa03a18d9
kemokemo <t2wonderland@gmail.com>
2023-02-13 22:03:23 +0900

    chore: change CI settings.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,20 +9,38 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+var (
+	ver bool
+)
+
+func init() {
+	flag.BoolVar(&ver, "v", false, "display version")
+	flag.Parse()
+}
+
 func main() {
+	os.Exit(run())
+}
+
+func run() int {
+	if ver {
+		fmt.Fprintf(os.Stdout, "diskusage version %s.%s\n", Version, Revision)
+		return 0
+	}
+
 	var free, total, avail uint64
 
 	path := "c:\\"
 	pathPtr, err := windows.UTF16PtrFromString(path)
 	if err != nil {
 		fmt.Println("failed to convert the path: ", err)
-		os.Exit(1)
+		return 1
 	}
 
 	err = windows.GetDiskFreeSpaceEx(pathPtr, &free, &total, &avail)
 	if err != nil {
 		fmt.Println("failed to get free space: ", err)
-		os.Exit(1)
+		return 1
 	}
 
 	fmt.Println("## IEC size (like Windows)")
@@ -29,4 +48,6 @@ func main() {
 
 	fmt.Println("## SI size")
 	fmt.Printf("Total: %v, Free: %v, Available: %v\n", humanize.Bytes(total), humanize.Bytes(free), humanize.Bytes(avail))
+
+	return 0
 }
